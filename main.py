be010acdae9b4ef3285e884fd28bea8e49c64d99
AylinNaebzadeh <a_naeb@comp.iust.ac.ir>
2024-05-23 09:36:26 +0330

    [add] : `XLM-RoBERTa-FineTuned-With-Dreaddit` model for stress detection

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import config
 from chatbot.chatbot import send_message
-
+from disorder_detector.stress_detector import check_for_stress_in_text
 import openai
 
 openai.api_key = config.OPENAI_API_KEY
 
+
 def main():
+    input_text = "سلام وقت‌بخیر"
+    check_for_stress_in_text(input_text)
     print(send_message(message=''))
+
 
 if __name__ == '__main__':
     main()
